88d07f9df789c5ad819405d21106573062765cd2
Surya Permana <suryapermana92@gmail.com>
2017-09-21 23:56:51 +0700

    Initial commit

--- index.android.js
+++ index.android.js
@@ -2,11 +2,16 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry, Text } from 'react-native';
-import Header from './src/components/header.js';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
 // Create a component
 const App = () => (
-  <Header />
+  <View style={{ flex:1}}>
+  <Header headerText={'Albums'} />
+  <AlbumList />
+  </View>
 )
 
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,34 @@
 // Import libraries for making a component
-
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
-	return <Text>Albums!</Text>;
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+ 	<View style={viewStyle}>
+ 	<Text style={textStyle}>{props.headerText}</Text>
+ 	</View>
+ 	)
 };
 
-// Add comment
-export default Header
+const styles = {
+	viewStyle: {
+	backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    paddingTop: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 5,
+    position: 'relative'
+	},
+  textStyle: {
+	fontSize: 20
+  }
+  };
+
+// Make the component available to other parts of the app
+export default Header;
